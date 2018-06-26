1a59d25b4b739e84a5bfa6479422ead87ca7e885
Sagar Raj <39821848+ShoolinLabs@users.noreply.github.com>
2018-06-26 16:57:43 +0530

    Add files via upload

---
+++ gps.ino
@@ -0,0 +1,39 @@
+
+long lat, lon;
+boolean gotGPS;
+
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+SoftwareSerial GPS(4, 5); // create gps sensor connection
+TinyGPS gps;
+void setup()
+{
+  Serial.begin(9600);
+  GPS.begin(9600);
+}
+void loop()
+{
+  
+
+      byte a;
+
+      if (GPS.available())
+      {
+
+        a = GPS.read();
+        while (gps.encode(a))
+        {
+          gps.get_position(&lat, &lon);
+          Serial.print("Position: ");
+          Serial.print("\n ");
+          Serial.print("lat ");
+          Serial.print((lat * 0.000001), 8);
+          Serial.print(", ");
+          Serial.print("lon ");
+          Serial.println((lon * 0.000001), 8);
+          Serial.print("\n");
+         
+  
+
+      }}}
