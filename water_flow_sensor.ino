e7f88da2357bf11264295d690780a60e3736b8be
Sagar Raj <39821848+ShoolinLabs@users.noreply.github.com>
2018-06-27 17:29:25 +0530

    Add files via upload

---
+++ water_flow_sensor.ino
@@ -0,0 +1,49 @@
+
+byte statusLed    = 13;
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin       = 2;
+float calibrationFactor = 4.5;
+volatile byte pulseCount;  
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+unsigned long oldTime;
+void setup(){
+  Serial.begin(38400);
+  pinMode(statusLed, OUTPUT);
+  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);}
+void loop()
+{ if((millis() - oldTime) > 1000)    // Only process counters once per second
+  { detachInterrupt(sensorInterrupt);
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    oldTime = millis();
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+    unsigned int frac;
+    Serial.print("Flow rate: ");
+    Serial.print(int(flowRate));  // Print the integer part of the variable
+    Serial.print(".");             // Print the decimal point
+    // Determine the fractional part. The 10 multiplier gives us 1 decimal place.
+    frac = (flowRate - int(flowRate)) * 10;
+    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+    Serial.print("L/min");
+    Serial.print("  Current Liquid Flowing: ");             // Output separator
+    Serial.print(flowMilliLitres);
+    Serial.print("mL/Sec");
+    Serial.print("  Output Liquid Quantity: ");             // Output separator
+    Serial.print(totalMilliLitres);
+    Serial.println("mL"); 
+    pulseCount = 0;
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);}}
+void pulseCounter()
+{
+  pulseCount++;
+}
