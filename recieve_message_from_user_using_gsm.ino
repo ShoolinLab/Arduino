827eb7d4fdea15f1fab0adf2d6593170d21d81c6
Sagar Raj <39821848+ShoolinLabs@users.noreply.github.com>
2018-10-10 19:36:32 +0530

    Add files via upload

---
+++ recieve_message_from_user_using_gsm.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(9, 10);
+
+void setup()
+{
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
+  mySerial.println("AT+CMGF=1"); 
+  delay(1000);
+}
+
+
+void loop()
+{
+  if(mySerial.available()>0)
+  {
+    Serial.print("ddff");
+    String data=mySerial.readString();
+    Serial.print(data);
+  }
+}
+
+  
