8e1b535bdbe9c744c918e4689c2b1cb564ea12de
Sagar Raj <39821848+ShoolinLabs@users.noreply.github.com>
2018-10-31 00:07:11 +0530

    Add files via upload
    with the help of this program you can control appliances  and save energy .while n no. of people enter it get on and same leave it get off

---
+++ IR_LCD_counter.ino
@@ -0,0 +1,45 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(8,9,4,5,6,7);
+
+#define IR2 2
+#define IR3 3
+#define del 1000
+#define led 13
+
+int count=0;
+
+void setup() {
+  pinMode(IR2,INPUT);
+  pinMode(IR3,INPUT);
+  pinMode(led,OUTPUT);
+  lcd.begin(16,2);
+}
+
+void loop() {
+  int bs2= digitalRead(IR2); 
+  int bs3= digitalRead(IR3); 
+
+  if(bs2==HIGH){
+    count++;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Person:");
+    lcd.print(count);
+    delay(del);
+  }
+  else if(bs3==HIGH){
+    count--;
+    lcd.clear();
+    lcd.setCursor(0,0);  
+    lcd.print("Person:");
+    lcd.print(count);
+    delay(del);
+  }
+    
+  if(count==0){
+    digitalWrite(led,LOW);
+  }
+  else{
+    digitalWrite(led,HIGH);    
+  }
+}
